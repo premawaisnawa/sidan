76fad41c8c545e0e98ea0e0f79410643486587ef
Buddy <buddyy93@gmail.com>
2018-05-21 13:33:39 +0800

    add more frontend views and edited some views

---
+++ application/views/frontend/antrian.php
@@ -0,0 +1,78 @@
+<section class="my-5 container">
+    <div class="row">
+        <div class="col-md-2">
+            <img class="img-fluid" src="public/images/telkomsel.png">
+        </div>
+        <div class="col-md-2 my-3">
+            <p class="font-weight-bold">Username : Test</p>
+            <p class="font-weight-bold">Category : Grapari</p>
+            <p class="font-weight-bold">No. Antrian : A03</p>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-md-12">
+            <p class="h4 text-center">Daftar Antrian</p>
+        </div>
+        <div class="col-md-4 ml-auto my-3">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Masukan kata kunci..">
+                <div class="input-group-append">
+                    <button class="btn btn-primary" type="button"><i class="fa fa-search mr-2"></i>Cari</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12">
+            <table class="table table-hover">
+                <thead>
+                <tr class="text-center">
+                    <th scope="col">No</th>
+                    <th scope="col">No. Antrian</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Tanggal</th>
+                </tr>
+                </thead>
+                <tbody class="text-center">
+                <tr>
+                    <th scope="row">1</th>
+                    <td>A01</td>
+                    <td>Budiman</td>
+                    <td>2018-06-01, 03:00 PM</td>
+                </tr>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>A02</td>
+                    <td>Kade Prema Waisnawa</td>
+                    <td>2018-06-01, 04:00 PM</td>
+                </tr>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>A03</td>
+                    <td>Nugie Nugraha</td>
+                    <td>2018-06-01, 05:00 PM</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-md-12 mt-4">
+            <nav aria-label="...">
+                <ul class="pagination justify-content-center">
+                    <li class="page-item disabled">
+                        <span class="page-link">Previous</span>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item active">
+      <span class="page-link">
+        2
+        <span class="sr-only">(current)</span>
+      </span>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                        <a class="page-link" href="#">Next</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</section>
---
+++ application/views/frontend/partner-minisite-request.php
@@ -0,0 +1,45 @@
+<section class="my-5 container">
+    <div class="row">
+        <div class="col-md-2 mx-auto">
+            <img class="img-fluid" src="<?php echo base_url()?>public/images/telkomsel.png">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <h4 class="text-center my-2">Selamat datang di Telkomsel Minisite</h4>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, beatae culpa cupiditate dicta
+                distinctio dolores ducimus eos error, esse labore laboriosam maiores modi molestiae nemo odio quas quod
+                rem repellendus similique sunt. Architecto cupiditate dolor perspiciatis quia ratione rem sed?</p>
+        </div>
+    </div>
+    <div class="wow fadeInUp">
+        <div class="row my-3">
+            <div class="col-md-3 mx-auto rounded sidan-shadow p-4">
+                <img class="img-fluid w-100" src="<?php echo base_url()?>public/images/grapari.png">
+            </div>
+            <div class="col-md-3 mx-auto rounded sidan-shadow p-4">
+                <img class="img-fluid w-100" src="<?php echo base_url()?>public/images/indihome.png">
+            </div>
+            <div class="col-md-3 mx-auto rounded sidan-shadow p-4">
+                <img class="img-fluid w-100" src="<?php echo base_url()?>public/images/telkomsel.png">
+            </div>
+        </div>
+        <div class="d-flex justify-content-around align-items-center my-3">
+            <div class="w-25 text-center">
+                <button class="btn btn-dark text-white"><i
+                        class="fa fa-send mr-2"></i>Ambil
+                </button>
+            </div>
+            <div class="w-25 text-center">
+                <button class="btn btn-dark text-white"><i
+                        class="fa fa-send mr-2"></i>Ambil
+                </button>
+            </div>
+            <div class="w-25 text-center">
+                <button class="btn btn-dark text-white"><i
+                        class="fa fa-send mr-2"></i>Ambil
+                </button>
+            </div>
+        </div>
+    </div>
+</section>
