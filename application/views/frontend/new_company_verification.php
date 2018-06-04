f0bd620590876893a16cfe2a17ccab7013761e56
buddyy93 <buddyy93@gmail.com>
2018-06-04 19:59:41 +0800

    redesign verification pages and request ticket page

--- application/views/frontend/new_company_verification.php
+++ application/views/frontend/new_company_verification.php
@@ -1,43 +1,72 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <form class="" action="<?php echo base_url().'index.php/Registration/verify_company_account'; ?>" method="post">
-      <input type="hidden" name="company_code" value="<?php echo $user[0]->Code; ?>">
-      <div class="">
-        <label for="">Email</label>
-      <input type="email" name="email" value="<?php echo $user[0]->Email; ?>" disabled>
-      </div>
-      <div class="">
-        <label for="">Phone Number</label>
-      <input type="text" name="phone_number" value="<?php echo $user[0]->PhoneNumber; ?>" >
-      </div>
-      <div class="">
-        <label for="">Company Name</label>
-      <input type="text" name="company_name" value="<?php echo $user[0]->CompanyName; ?>">
-      </div>
-      <div class="">
-        <label for="">First Name</label>
-      <input type="text" name="first_name" value="<?php echo $user[0]->FirstName; ?>">
-      </div>
-      <div class="">
-        <label for="">Last Name</label>
-      <input type="text" name="last_name" value="<?php echo $user[0]->LastName; ?>">
-      </div>
-      <div class="">
-        <label for="">Password</label>
-      <input type="password" name="password" value="<?php echo $user[0]->Password; ?>">
-      </div>
-      <div class="">
-        <label for="">Confirm Password</label>
-      <input type="password" name="c_password" value="">
-      </div>
-      <div class="">
-      <button type="submit" name="button">Submit</button>
-      </div>
-    </form>
-  </body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Selamat Datang di SIDAN</title>
+    <link rel="stylesheet" href="<?php echo base_url() ?>/public/css/styles.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>/public/css/font-awesome.min.css">
+
+    <script src="<?php echo base_url() ?>/public/js/bundle.js"></script>
+    <script
+            src="https://code.jquery.com/jquery-3.3.1.min.js"
+            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+            crossorigin="anonymous"></script>
+    <script src="<?php echo base_url() ?>/public/js/wow.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            new WOW().init();
+        });
+    </script>
+</head>
+<body class="bg-light">
+<section class="container my-5">
+    <div class="row">
+        <div class="col-md-5 mx-auto">
+            <div class="card border-dark mb-3 w-100 sidan-shadow">
+                <div class="card-header sidan-blue"></div>
+                <div class="card-body text-dark">
+                    <h4 class="text-center">SIDAN</h4>
+                    <h5 class="card-title text-center">Verifikasi Akun Company</h5>
+                    <form class="" action="<?php echo base_url() . 'index.php/Registration/verify_company_account'; ?>"
+                          method="post">
+                        <input type="hidden" name="company_code" value="<?php echo $user[0]->Code; ?>">
+                        <div class="form-group">
+                            <label for="">Email</label>
+                            <input class="form-control" type="email" name="email" value="<?php echo $user[0]->Email; ?>" disabled>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Phone Number</label>
+                            <input class="form-control" type="text" name="phone_number" value="<?php echo $user[0]->PhoneNumber; ?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Company Name</label>
+                            <input class="form-control" type="text" name="company_name" value="<?php echo $user[0]->CompanyName; ?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="">First Name</label>
+                            <input class="form-control" type="text" name="first_name" value="<?php echo $user[0]->FirstName; ?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Last Name</label>
+                            <input class="form-control" type="text" name="last_name" value="<?php echo $user[0]->LastName; ?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Password</label>
+                            <input class="form-control" type="password" name="password" value="<?php echo $user[0]->Password; ?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Confirm Password</label>
+                            <input class="form-control" type="password" name="c_password" value="">
+                        </div>
+                        <div class="text-center">
+                            <button class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
 </html>
--- application/views/frontend/request_ticket_success.php
+++ application/views/frontend/request_ticket_success.php
@@ -1,14 +1,47 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <h1>TicketCode :<?php echo $service[0]->TicketCode; ?></h1>
-    <h1>Date :<?php echo $service[0]->StartTime; ?></h1>
-    <h1>Email :<?php echo $service[0]->CustomerEmail; ?></h1>
-    <h1>Phone :<?php echo $service[0]->PhoneNumber; ?></h1>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Selamat Datang di SIDAN</title>
+    <link rel="stylesheet" href="<?php echo base_url() ?>/public/css/styles.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>/public/css/font-awesome.min.css">
 
-  </body>
+    <script src="<?php echo base_url() ?>/public/js/bundle.js"></script>
+    <script
+            src="https://code.jquery.com/jquery-3.3.1.min.js"
+            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+            crossorigin="anonymous"></script>
+    <script src="<?php echo base_url() ?>/public/js/wow.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            new WOW().init();
+        });
+    </script>
+</head>
+<body class="bg-light">
+<section class="container my-5">
+    <div class="row">
+        <div class="col-md-5 mx-auto">
+            <div class="card border-dark mb-3 w-100 sidan-shadow">
+                <div class="card-header sidan-blue"></div>
+                <div class="card-body text-dark">
+                    <h4 class="text-center">SIDAN</h4>
+                    <h5 class="card-title text-center">Request Ticket Success</h5>
+                    <div class="text-center">
+                        <p class="font-weight-bold">TicketCode</p>
+                        <p><?php echo $service[0]->TicketCode; ?></p>
+                        <p class="font-weight-bold">Date</p>
+                        <p><?php echo $service[0]->StartTime; ?></p>
+                        <p class="font-weight-bold">Email</p>
+                        <p><?php echo $service[0]->CustomerEmail; ?></p>
+                        <p class="font-weight-bold">Phone</p>
+                        <p><?php echo $service[0]->PhoneNumber; ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
 </html>
